4e006b6e0cf3bfcc7257743dc61d396af042f1ec
tdergouziv2 <tdergouzi@gmail.com>
2021-12-22 16:56:21 +0800

    feat: impl GameNFT.sol

--- contracts/GameNFT.sol
+++ contracts/GameNFT.sol
@@ -7,6 +7,7 @@ import './modules/Configable.sol';
 import './modules/WhiteList.sol';
 import './libraries/SafeMath.sol';
 import './libraries/Strings.sol';
+import "./libraries/Base64.sol";
 import './libraries/TransferHelper.sol';
 import './ERC721/extensions/ERC721Enumerable.sol';
 
@@ -14,40 +15,47 @@ contract GameNFT is ERC721Enumerable, Configable, WhiteList {
     using Strings for uint256;
     using SafeMath for uint256;
 
-    bool public isLocked;
     string public baseURI;
     string public imgSuffix;
     uint256 public immutable maxSupply;
 
-    event Lock();
     event NonFungibleTokenRecovery(address indexed token, uint256 tokenId);
     event TokenRecovery(address indexed token, uint256 amount);
 
     constructor(string memory _name, string memory _symbol, uint256 _maxSupply) public {
+        require(_maxSupply > 0, 'GameNFT: Invalid max supply');
         owner = msg.sender;
         name = _name;
         symbol = _symbol;
         maxSupply = _maxSupply;
     }
 
-    function lock() external onlyOwner {
-        require(!isLocked, "GameNFT: Contract is locked");
-        isLocked = true;
-        emit Lock();
+    function setWhiteList(address _addr, bool _value) external override onlyDev {
+        _setWhiteList(_addr, _value);
+    }
+        
+    function setWhiteLists(address[] calldata _addrs, bool[] calldata _values) external override onlyDev {
+        require(_addrs.length == _values.length, 'GameNFT: Invalid param');
+        for(uint i; i<_addrs.length; i++) {
+            _setWhiteList(_addrs[i], _values[i]);
+        }
     }
 
-    function mint(address _to) external onlyWhiteList returns(uint256) {
-        require(!isLocked, "GameNFT: Contract is locked");
-        require(totalSupply() < maxSupply, "GameNFT: total supply reached");
-        uint tokenId = totalSupply().add(1);
-        _mint(_to, tokenId);
-        return tokenId;
-    }
-
-    function setUriInfo(string memory _baseURI, string memory _imgSuffix) external onlyOwner {
-        require(!isLocked, "GameNFT: Contract is locked");
+    function setUriInfo(string memory _baseURI, string memory _imgSuffix) external onlyDev {
         baseURI = _baseURI;
         imgSuffix = _imgSuffix;
+    }
+
+    function recoverNonFungibleToken(address _token, uint256 _tokenId) external onlyDev {
+        IERC721(_token).transferFrom(address(this), address(msg.sender), _tokenId);
+        emit NonFungibleTokenRecovery(_token, _tokenId);
+    }
+
+    function recoverToken(address _token) external onlyDev {
+        uint256 balance = IERC20(_token).balanceOf(address(this));
+        require(balance != 0, "Operations: Cannot recover zero balance");
+        TransferHelper.safeTransfer(_token, address(msg.sender), balance);
+        emit TokenRecovery(_token, balance);
     }
 
     function tokensOfOwnerBySize(address user, uint256 cursor, uint256 size) external view returns (uint256[] memory, uint256) {
@@ -55,29 +63,33 @@ contract GameNFT is ERC721Enumerable, Configable, WhiteList {
         if (length > balanceOf(user) - cursor) {
             length = balanceOf(user) - cursor;
         }
-
         uint256[] memory values = new uint256[](length);
         for (uint256 i = 0; i < length; i++) {
             values[i] = tokenOfOwnerByIndex(user, cursor + i);
         }
-
         return (values, cursor + length);
     }
 
-    function tokenURI(uint256 _tokenId) public view override returns (string memory) {
-        require(_exists(_tokenId), "GameNFT: URI query for nonexistent token");
-        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, _tokenId.toString(), imgSuffix)) : "";
+    function imgURI(uint256 _tokenId) public view returns (string memory) {
+        return string(abi.encodePacked(baseURI, _tokenId.toString(), imgSuffix));
     }
 
-    function recoverNonFungibleToken(address _token, uint256 _tokenId) external onlyOwner {
-        IERC721(_token).transferFrom(address(this), address(msg.sender), _tokenId);
-        emit NonFungibleTokenRecovery(_token, _tokenId);
+    function tokenURI(uint256 _tokenId) override public view returns (string memory) {
+        string memory json = Base64.encode(bytes(string(abi.encodePacked('{"name": "', name, ' #', _tokenId.toString(), '", "description": "', symbol, '", "image": "', imgURI(_tokenId), '"}'))));
+        return string(abi.encodePacked('data:application/json;base64,', json));
     }
 
-    function recoverToken(address _token) external onlyOwner {
-        uint256 balance = IERC20(_token).balanceOf(address(this));
-        require(balance != 0, "Operations: Cannot recover zero balance");
-        TransferHelper.safeTransfer(_token, address(msg.sender), balance);
-        emit TokenRecovery(_token, balance);
+    function mint(address _to) public onlyWhiteList returns(uint256) {
+        require(totalSupply() < maxSupply, "GameNFT: Total supply reached");
+        uint tokenId = totalSupply().add(1);
+        _mint(_to, tokenId);
+        return tokenId;
+    }
+
+    function mint(address[] memory _users) external onlyWhiteList {
+        require(_users.length > 0, "GameNFT: Invalid users");
+        for(uint i = 0; i < _users.length; i++) {
+            mint(_users[i]);
+        }
     }
 }
